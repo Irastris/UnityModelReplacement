c2c982a3db9dbae400a1123e452384b2c52fbefc
Irastris <irastris15@gmail.com>
2025-07-06 21:59:05 -0400

    RC1

---
+++ CharacterReplacer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace UnityModelReplacement;
+
+public class CharacterReplacer : MonoBehaviour
+{
+    public GameObject replacementModel;
+    public SkinnedMeshRenderer characterRenderer;
+    public SkinnedMeshRenderer replacementRenderer;
+    public Transform targetRoot;
+    public Transform sourceRoot;
+    public string rootName = "Hip";
+
+    public void LoadModel(string assetPath)
+    {
+        replacementModel = Instantiate(Plugin.ModBundle.LoadAsset<GameObject>(assetPath));
+        replacementModel.transform.localScale = Vector3.one * 1.2121f; // Bandaid for me fucking up when preparing my Blender project
+        replacementRenderer = replacementModel.transform.Find("Model").GetComponent<SkinnedMeshRenderer>();
+        replacementRenderer.updateWhenOffscreen = true;
+
+        foreach (Renderer renderer in this.gameObject.GetComponentsInChildren<Renderer>(true))
+        {
+            renderer.forceRenderingOff = true;
+        }
+
+        foreach (Material material in replacementRenderer.materials)
+        {
+            material.shader = Shader.Find("W/Character");
+            material.DisableKeyword("_USESKINTONE_ON");
+            if (this.gameObject.GetComponent<Character>().IsLocal)
+            {
+                material.SetFloat("_VertexGhost", 1f);
+            }
+        }
+    }
+
+    public void Awake()
+    {
+        characterRenderer = this.transform.Find("Scout/MainMesh").GetComponent<SkinnedMeshRenderer>();
+
+        if (Plugin.ModBundle == null || !characterRenderer)
+        {
+            Destroy(this);
+        }
+
+        switch (this.gameObject.GetComponent<Character>().characterName.ToLower())
+        {
+            case "spongebob":
+            case "spongebob squarepants":
+                LoadModel("Assets/_Modding/SpongeBob.prefab");
+                break;
+            case "patrick":
+            case "patrick star":
+                LoadModel("Assets/_Modding/Patrick.prefab");
+                break;
+            case "eugene":
+            case "eugene krabs":
+            case "krabs":
+            case "mr. krabs":
+                LoadModel("Assets/_Modding/MrKrabs.prefab");
+                break;
+            case "squidward":
+            case "squidward tentacles":
+                LoadModel("Assets/_Modding/Squidward.prefab");
+                break;
+            case "plankton":
+                LoadModel("Assets/_Modding/Plankton.prefab");
+                break;
+            case "sandy":
+            case "sandy cheeks":
+                LoadModel("Assets/_Modding/Sandy.prefab");
+                break;
+            default:
+                Destroy(this);
+                break;
+        }
+    }
+
+    public Transform GetBoneTransformFromRenderer(SkinnedMeshRenderer renderer, string boneName)
+    {
+        Transform bone = Array.Find(renderer.bones, bone => bone.name == boneName);
+
+        return bone;
+    }
+
+    public void CopyPose()
+    {
+        if (characterRenderer == null || replacementRenderer == null)
+        {
+            return;
+        }
+
+        targetRoot = GetBoneTransformFromRenderer(replacementRenderer, rootName);
+        sourceRoot = GetBoneTransformFromRenderer(characterRenderer, rootName);
+        targetRoot.position = sourceRoot.position;
+
+        foreach (Transform targetBone in replacementRenderer.bones)
+        {
+            Transform sourceBone = GetBoneTransformFromRenderer(characterRenderer, targetBone.name);
+            targetBone.rotation = sourceBone.rotation;
+        }
+
+        return;
+    }
+
+    public void LateUpdate()
+    {
+        CopyPose();
+    }
+
+    public void OnDestroy()
+    {
+        Debug.Log($"CharacterRenderer attached to {this.gameObject.name} was destroyed!");
+        CancelInvoke();
+        Destroy(replacementModel);
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,83 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.Mono;
+using HarmonyLib;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+namespace UnityModelReplacement;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("PEAK.exe")]
+public class Plugin : BaseUnityPlugin
+{
+    public static Plugin Instance = null;
+    public static AssetBundle ModBundle = null;
+
+    public void LoadAssetBundle()
+    {
+        MemoryStream memoryStream;
+
+        using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("UnityModelReplacement.mods.bundle"))
+        {
+            memoryStream = new MemoryStream((int)stream.Length);
+            stream.CopyTo(memoryStream);
+        }
+
+        ModBundle = AssetBundle.LoadFromMemory(memoryStream.ToArray());
+    }
+
+    public void Awake()
+    {
+        if (Instance == null) Instance = this;
+
+        LoadAssetBundle();
+
+        Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+    }
+    
+    [HarmonyPatch(typeof(Character), "Awake")]
+    public class Patch_Character_Awake
+    {
+        public static void Postfix(ref MonoBehaviour __instance)
+        {
+            Debug.Log("Postfix is firing on method Awake from class Character!");
+            if (!__instance.gameObject.TryGetComponent(out CharacterReplacer existingCharacterReplacer))
+            {
+                __instance.gameObject.AddComponent<CharacterReplacer>();
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(BingBong), "Start")]
+    public class Patch_BingBong_Start
+    {
+        public static void Postfix(ref MonoBehaviour __instance)
+        {
+            if (ModBundle != null)
+            {
+                Mesh garyMesh = ModBundle.LoadAsset<Mesh>("Assets/_Modding/Gary.mesh");
+                Texture2D garyTex = ModBundle.LoadAsset<Texture2D>("Assets/_Modding/Gary.png");
+
+                foreach (MeshRenderer renderer in __instance.transform.GetComponentsInChildren<MeshRenderer>(true))
+                {
+                    if (renderer.transform.name == "Cube")
+                    {
+                        MeshFilter meshFilter = renderer.transform.GetComponent<MeshFilter>();
+                        meshFilter.sharedMesh = garyMesh;
+                        renderer.material.SetTexture("_BaseTexture", garyTex);
+                    }
+                    else
+                    {
+                        renderer.forceRenderingOff = true;
+                    }
+                }
+            }
+        }
+    }
+}
